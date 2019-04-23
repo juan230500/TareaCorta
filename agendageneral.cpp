accb0bb3429d90ebea818f74dfa10cd7287d09c4
Juan <juanpo.alvarado@gmail.com>
2019-04-22 22:57:25 -0600

    Agenda general

---
+++ agendageneral.cpp
@@ -0,0 +1,48 @@
+#include "agendageneral.h"
+
+AgendaGeneral::AgendaGeneral()
+{
+
+}
+
+void AgendaGeneral::sacar(int procesoactual,int vehiculo)
+{
+    for (int i=0;i<3;i++){
+        if (procesos[procesoactual][i]==vehiculo){
+            procesos[procesoactual][i]=0;
+            return;
+        }
+    }
+}
+
+void AgendaGeneral::meter(int procesoactual, int vehiculo)
+{
+    for (int i=0;i<3;i++){
+        if (procesos[procesoactual][i]==0){
+            procesos[procesoactual][i]=vehiculo;
+            return;
+        }
+    }
+    procesos[procesoactual][0]=vehiculo;
+}
+
+bool AgendaGeneral::consultar(int procesoactual, int vehiculo)
+{
+    for (int i=0;i<3;i++){
+        if (procesos[procesoactual][i]==vehiculo){
+            return true;
+        }
+    }
+    return false;
+}
+
+void AgendaGeneral::print()
+{
+    for (int i=0;i<6;i++){
+        qDebug()<<"Proceso"<<i<<"==============";
+        for (int j=0;j<3;j++){
+            qDebug()<<procesos[i][j];
+        }
+    }
+    qDebug()<<"Fin del reporte"<<"==============";
+}
---
+++ agendageneral.h
@@ -0,0 +1,36 @@
+#ifndef AGENDAGENERAL_H
+#define AGENDAGENERAL_H
+#include "qdebug.h"
+
+class AgendaGeneral
+{
+private:
+    int procesos[6][3]={}; //[LetraProceso][campo]
+public:
+    AgendaGeneral();
+    /**
+     * @brief sacar saca un vehiculo de un proceso
+     * @param proceso
+     * @param vehiculo
+     */
+    void sacar(int proceso,int vehiculo);
+    /**
+     * @brief meter mete un vehiculo a un proceso
+     * @param proceso
+     * @param vehiculo
+     */
+    void meter(int proceso, int vehiculo);
+    /**
+     * @brief consultar consulta si un vehiculo está en un proceso
+     * @param proceso
+     * @param vehiculo
+     * @return
+     */
+    bool consultar(int proceso, int vehiculo);
+    /**
+     * @brief print imprime el estado de los procesos
+     */
+    void print();
+};
+
+#endif // AGENDAGENERAL_H
